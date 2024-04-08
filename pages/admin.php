c68793a4dbe517573466482188801c4c5d5c212f
Mehdi <mehdi@laptop14.local>
2024-04-08 14:14:45 +0200

    1.1

--- pages/admin.php
+++ pages/admin.php
@@ -1,17 +1,23 @@
 <?php
-include('../header/header.php')
-?>
+include('../header/header.php');
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./admin.css"/>
-    <title>Admin</title>
-</head>
-<body>
-    <h1>Welcome to the Admin page </h1>
-    <h3>You did it</h3>
-</body>
-</html>
+if (isset($_SESSION['id']))
+{?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./admin.css"/>
+        <title>Admin</title>
+    </head>
+    <body>
+        <h1>Welcome to the Admin page </h1>
+        <h3>You did it</h3>
+    </body>
+    </html><?php
+}
+else {
+    header('location: ../treatment/error_no-permission.php');
+}
---
+++ treatment/error_no-permission.php
@@ -0,0 +1,15 @@
+<?php
+include('../header/header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>No permission</title>
+</head>
+<body>
+    <h1>YOU DO NOT HAVE THE PERMISSION</h1>
+</body>
+</html>
